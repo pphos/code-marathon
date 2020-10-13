9dc358d82d55c77289630ed701dedbbab5940c64
pphos <coc4h8conhc6h12nh@gmail.com>
2020-10-13 20:55:10 +0900

    add rust atcoder solution

---
+++ rust/abc086_a/target/rls/debug/deps/abc086_a-f23003e7c75dd552.d
@@ -0,0 +1,5 @@
+/home/phos/Downloads/atcoder/rust/abc086_a/target/rls/debug/deps/abc086_a-f23003e7c75dd552.rmeta: src/main.rs
+
+/home/phos/Downloads/atcoder/rust/abc086_a/target/rls/debug/deps/abc086_a-f23003e7c75dd552.d: src/main.rs
+
+src/main.rs:
