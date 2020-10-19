81b320ff5d8591075262c354ba9f029d36116862
pphos <coc4h8conhc6h12nh@gmail.com>
2020-10-19 12:53:20 +0900

    add Coins solutions

---
+++ rust/abc087_b/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn main() {
+    let a: u32 = read();
+    let b: u32 = read();
+    let c: u32 = read();
+    let x: u32 = read();
+
+    let mut ans: u32 = 0;
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                if 500 * i + 100 * j + 50 * k == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    println!("{:?}", ans);
+
+}
